4fa129ba069e87d43ea7e6919a3e5ad1ee6dc4d7
Jeff Patzer <jeff.patzer@moovweb.com>
2015-02-02 11:09:16 -0800

    make it a server only method

--- hipaa.logging.js
+++ hipaa.logging.js
@@ -1,34 +1,38 @@
-HipaaLogger = {
-  logEvent: function(eventType, userId, collectionName, recordId, message, patientId, patientName){
-    console.log('logEvent', eventType, userId, collectionName, recordId, message, patientId, patientName);
-    
-    var newRecord = {
-      timestamp: new Date(),
-      type: eventType
-    };
+if (Meteor.isServer) {
+  
+  HipaaLogger = {
+    logEvent: function(eventType, userId, collectionName, recordId, message, patientId, patientName){
+      console.log('logEvent', eventType, userId, collectionName, recordId, message, patientId, patientName);
+      
+      var newRecord = {
+        timestamp: new Date(),
+        type: eventType
+      };
 
-    if(userId){
-      newRecord.userId = userId;
-    }
-    // if(userName){
-    //   newRecord.userName = userName;
-    // }
-    if(recordId){
-      newRecord.recordId = recordId;
-    }
-    if(collectionName){
-      newRecord.collectionName = collectionName;
-    }
-    if(message){
-      newRecord.message = message;
-    }
-    if(patientId){
-      newRecord.patientId = patientId;
-    }
-    if(patientName){
-      newRecord.patientName = patientName;
-    }
+      if(userId){
+        newRecord.userId = userId;
+      }
+      // if(userName){
+      //   newRecord.userName = userName;
+      // }
+      if(recordId){
+        newRecord.recordId = recordId;
+      }
+      if(collectionName){
+        newRecord.collectionName = collectionName;
+      }
+      if(message){
+        newRecord.message = message;
+      }
+      if(patientId){
+        newRecord.patientId = patientId;
+      }
+      if(patientName){
+        newRecord.patientName = patientName;
+      }
 
-    console.log(Hipaa.insert(newRecord));
-  }
-};
+      console.log(Hipaa.insert(newRecord));
+    }
+  };
+
+}
